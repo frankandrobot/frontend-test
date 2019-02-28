8897de2c31adbb015dbdb25bc557e01144232d8d
Uriel Avalos <uri@frankandrobot.com>
2019-02-28 12:58:30 -0500

    [comp] actually added theme file

---
+++ src/components/theme.js
@@ -0,0 +1,13 @@
+export default {
+  fontFamily: `"Helvetica Neue", Helvetica, sans-serif`,
+  fontWeightLight: 300,
+  colorHeading: "#333333",
+  colorText: "#666666",
+  bodyPaddingLeft: "64px",
+  bodyPaddingRight: "64px",
+  colorLine: "#e6e6e6",
+  /* filter */
+  colorFilterBy: "#606060",
+  fontSizeFilter: "16px",
+  marginRightFilter: "25px",
+};
