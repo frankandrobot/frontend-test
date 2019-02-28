a53b6cbb3da95f2f3e871114a0d843fa3d0b4e85
Uriel Avalos <uri@frankandrobot.com>
2019-02-27 23:39:38 -0500

    [meta] added babel

---
+++ babel.config.js
@@ -0,0 +1,38 @@
+const presets = [
+  [
+    "@babel/preset-env",
+    {
+      // when this is used, you don't need to explicitly import
+      // @babel/polyfill
+      useBuiltIns: "usage",
+      targets:
+        "last 3 chrome version and " +
+        "last 3 safari version and " +
+        "last 3 firefox version and " +
+        "ie 10 and " +
+        "ie 11 and " +
+        "edge >= 1",
+    },
+  ],
+  "@babel/preset-react",
+];
+
+const plugins = [
+  // this needs to always come first
+  [
+    "babel-plugin-styled-components",
+    {
+      pure: true,
+    },
+  ],
+  "react-hot-loader/babel",
+  // let [a, b, ...rest] = arr;
+  "@babel/plugin-transform-destructuring",
+  // makes writing classes simpler
+  "@babel/plugin-proposal-class-properties",
+];
+
+module.exports = {
+  presets,
+  plugins,
+};
