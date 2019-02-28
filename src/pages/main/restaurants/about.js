d7333ade709f0754b152491215568ebc39441b6d
Uriel Avalos <uri@frankandrobot.com>
2019-02-28 00:32:05 -0500

    [pages] added proper padding to about & main

--- src/pages/main/restaurants/about.js
+++ src/pages/main/restaurants/about.js
@@ -6,13 +6,19 @@ const Title = styled.h1`
   font-weight: 300;
   font-size: 54px;
   color: #333333;
+  margin: 0;
+  padding: 0;
+  margin-bottom: 24px;
 `;
 
-const Description = styled.div`
+const Description = styled.p`
   font-family: "Helvetica Neue", Helvetica, sans-serif;
   font-weight: 300;
   font-size: 22px;
   color: #666666;
+  margin: 0;
+  padding: 0;
+  margin-bottom: 36px;
 `;
 
 export default React.memo(function About() {
--- src/pages/main/restaurants/index.js
+++ src/pages/main/restaurants/index.js
@@ -4,14 +4,23 @@ import styled from "styled-components";
 import About from "./about";
 
 const Bar = styled.hr`
-  color: #e6e6e6;
+  color: #e6e6e6; /* old IE */
+  background-color: #e6e6e6;
+  border: none;
+  height: 1px;
+`;
+
+const Main = styled.main`
+  padding-top: 36px;
+  padding-left: 64px;
+  padding-right: 64px;
 `;
 
 export default function Resturants() {
   return (
-    <main className="restaurants">
+    <Main>
       <About />
       <Bar />
-    </main>
+    </Main>
   );
 }
