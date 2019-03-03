8ae63d0652750bdc94d862c3d25ee028bd957a34
Uriel Avalos <uri@frankandrobot.com>
2019-03-03 11:30:49 -0600

    [test] added Stars snapshot tests

--- src/components/ui/stars.test.js
+++ src/components/ui/stars.test.js
@@ -1,3 +1,6 @@
+import React from "react";
+import Stars from "./stars";
+import renderer from "react-test-renderer";
 import { toArray } from "./stars";
 
 test(`toArray converts 0 rating to [0,0,0,0]`, () => {
@@ -28,4 +31,16 @@ test(`toArray converts 3 rating to [1,1,1,0]`, () => {
 test(`toArray converts 3.5 rating to [1,1,1,0.5]`, () => {
   const result = toArray(3.5, 4);
   expect(result).toEqual([1, 1, 1, 0.5]);
+});
+
+test("Stars renders empty stars correctly", () => {
+  const component = renderer.create(<Stars rating={0} max={0} />);
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+test("Stars renders correctly", () => {
+  const component = renderer.create(<Stars rating={0} max={5} />);
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
 });
